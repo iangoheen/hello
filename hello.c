9569c226225c09d2f4df4d27d5f505bfb185a982
Prism019 <derektutaj@gmail.com>
2019-01-17 15:05:31 -0600

    Updated with my name and major; fixed syntax

--- hello.c
+++ hello.c
@@ -2,14 +2,15 @@
  * Author: Ian Goheen
  * Date:2019/01/15
  *
+ * Edited by Prism Tutaj on 2019/01/17
  */
 #include<stdlib.h>
 #include<stdio.h>
 
 int main(int argc, char **argv) {
-
 	printf("Ian Goheen\n");
-	printf("Major: Psychology\n";
-	
+	printf("Major: Psychology\n");
+	printf("Prism Tutaj\n");
+	printf("Major: Computer Science\n");
 	return 0;
 }	
