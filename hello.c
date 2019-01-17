1a4c7c7995c217c496516b1785f300b78be923f7
iangoheen <46796556+iangoheen@users.noreply.github.com>
2019-01-17 14:57:58 -0600

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Ian Goheen
+ * Date:2019/01/15
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+	printf("Ian Goheen\n");
+	printf("Major: Psychology\n";
+	
+	return 0;
+}	
